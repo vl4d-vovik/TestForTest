9283f713ea901f32d7cefa1a39b6ff56007a0004
VVV <vl4d.vovik@yandex.ru>
2019-07-22 21:21:56 +0300

    test !

--- src/main/java/TestForTest/App.java
+++ src/main/java/TestForTest/App.java
@@ -20,36 +20,36 @@ import java.net.URL;
 public class App 
 {
 
-    private WebDriver driver;
+/*    private WebDriver driver;
 
     @BeforeClass
     public void beforeTest() throws Exception{
 //        driver = new FirefoxDriver();
 //        driver = new ChromeDriЛver();
 
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), new ChromeOptions());
+        driver = new RemoteWebDriver(new URL("http://localhost:4545/wd/hub"), new ChromeOptions());
 
     }
     @AfterClass
     public void afterTest() {
         driver.quit();
-    }
+    }*/
 
     @Test
     public void test() {
 
 
-        System.out.println("test for test");
+        System.out.println("test for test!!!");
 //        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vvoitehovici\\IdeaProjects\\testdisportal\\drivers\\chromedriver.exe");
 //        driver = new ChromeDriver();
 
-        driver.get("https://www.google.com/");
+//        driver.get("https://www.google.com/");
 //        driver.get("http://demo.guru99.com/test/guru99home/");
-        String title = driver.getTitle();
+        //       String title = driver.getTitle();
 
 //        System.out.println(title);
 
-        Assert.assertTrue(title.contains("Demo Guru99 Page"));
+        //      Assert.assertFalse(title.contains("Demo Guru99 Page"));
 
 
     }
