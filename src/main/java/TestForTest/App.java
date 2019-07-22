5d77e6837c16f314b60f33f62e64cdab418b438d
VVV <vl4d.vovik@yandex.ru>
2019-07-22 14:12:44 +0300

    333

--- src/main/java/TestForTest/App.java
+++ src/main/java/TestForTest/App.java
@@ -1,6 +1,14 @@
 package TestForTest;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 /**
  * Hello world!
@@ -8,9 +16,34 @@ import org.junit.jupiter.api.Test;
  */
 public class App 
 {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void beforeTest() throws Exception{
+//        driver = new FirefoxDriver();
+//        driver = new ChromeDriver();
+
+//        driver = new RemoteWebDriver(new URL("http://localhost:4545"), new ChromeOptions());
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vvoitehovici\\IdeaProjects\\testdisportal\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+    @AfterEach
+    public void afterTest() {
+        driver.quit();
+    }
+
     @Test
     public void test() {
         System.out.println("test for test");
+
+
+        driver.get("https://www.google.com/");
+//        driver.get("http://demo.guru99.com/test/guru99home/");
+        String title = driver.getTitle();
+
+        Assertions.assertTrue(title.contains("Demo Guru99 Page"));
+
 
     }
 
